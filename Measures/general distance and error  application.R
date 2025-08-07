287e1a4d1498fdf8d4a6a45663416c139a0b2174
sachaisabell <76950563+sachaisabell@users.noreply.github.com>
2025-08-07 17:55:09 +0200

    Update general distance and error  application.R

--- Measures/general distance and error  application.R
+++ Measures/general distance and error  application.R
@@ -1,19 +1,35 @@
 
+#### for areal
+### MSE
+MSEA = c(sapply(UT_list[1:3], function(x) MSE_func(UT_list[[4]], x)),
+sapply(OV_list[1:3], function(x) MSE_func(OV_list[[4]], x)),
+sapply(NB_list[1:3], function(x) MSE_func(NB_list[[4]], x)))
+
+### MAE
+MAEA = c(sapply(UT_list[1:3], function(x) MAE_func(UT_list[[4]], x)),
+sapply(OV_list[1:3], function(x) MAE_func(OV_list[[4]], x)),
+sapply(NB_list[1:3], function(x) MAE_func(NB_list[[4]], x)))
+
+### SME
+SMEA = c(sapply(UT_list[1:3], function(x) SME_func(UT_list[[4]], x)),
+sapply(OV_list[1:3], function(x) SME_func(OV_list[[4]], x)),
+sapply(NB_list[1:3], function(x) SME_func(NB_list[[4]], x)))
+
+### Hellinger
+HDA = c(sapply(UT_list[1:3], function(x) HD_func(UT_list[[4]], x)),
+sapply(OV_list[1:3], function(x) HD_func(OV_list[[4]], x)),
+sapply(NB_list[1:3], function(x) HD_func(NB_list[[4]], x)))
+
+View(cbind(MSEA,SMEA,MAEA,HDA))
 
 
-
-
+#### for grid
 ### MSE
 sapply(normal_maps$raster_data[2:5], function(x) MSE_func(normal_maps$raster_data$original$count, x$count))
 sapply(clustered_maps$raster_data[2:5], function(x) MSE_func(clustered_maps$raster_data$original$count, x$count))
 sapply(random_maps$raster_data[2:5], function(x) MSE_func(random_maps$raster_data$original$count, x$count))
 sapply(smallclusters_maps$raster_data[2:5], function(x) MSE_func(smallclusters_maps$raster_data$original$count, x$count))
 sapply(consistent_maps$raster_data[2:5], function(x) MSE_func(consistent_maps$raster_data$original$count, x$count))
-
-sapply(UT_list[2:4], function(x) MSE_func(UT_list[[1]], x))
-sapply(OV_list[2:4], function(x) MSE_func(OV_list[[1]], x))
-sapply(NB_list[2:4], function(x) MSE_func(NB_list[[1]], x))
-
 
 ### MAE
 sapply(normal_maps$raster_data[2:5], function(x) MAE_func(normal_maps$raster_data$original$count, x$count))
@@ -22,21 +38,12 @@ sapply(random_maps$raster_data[2:5], function(x) MAE_func(random_maps$raster_dat
 sapply(smallclusters_maps$raster_data[2:5], function(x) MAE_func(smallclusters_maps$raster_data$original$count, x$count))
 sapply(consistent_maps$raster_data[2:5], function(x) MAE_func(consistent_maps$raster_data$original$count, x$count))
 
-sapply(UT_list[2:4], function(x) MAE_func(UT_list[[1]], x))
-sapply(OV_list[2:4], function(x) MAE_func(OV_list[[1]], x))
-sapply(NB_list[2:4], function(x) MAE_func(NB_list[[1]], x))
-
-
 ### SME
 sapply(normal_maps$raster_data[2:5], function(x) SME_func(normal_maps$raster_data$original$count, x$count))
 sapply(clustered_maps$raster_data[2:5], function(x) SME_func(clustered_maps$raster_data$original$count, x$count))
 sapply(random_maps$raster_data[2:5], function(x) SME_func(random_maps$raster_data$original$count, x$count))
 sapply(smallclusters_maps$raster_data[2:5], function(x) SME_func(smallclusters_maps$raster_data$original$count, x$count))
 sapply(consistent_maps$raster_data[2:5], function(x) SME_func(consistent_maps$raster_data$original$count, x$count))
-
-sapply(UT_list[2:4], function(x) SME_func(UT_list[[1]], x))
-sapply(OV_list[2:4], function(x) SME_func(OV_list[[1]], x))
-sapply(NB_list[2:4], function(x) SME_func(NB_list[[1]], x))
 
 
 ### Hellinger
@@ -46,16 +53,12 @@ sapply(random_maps$raster_data[2:5], function(x) HD_func(random_maps$raster_data
 sapply(smallclusters_maps$raster_data[2:5], function(x) HD_func(smallclusters_maps$raster_data$original$count, x$count))
 sapply(consistent_maps$raster_data[2:5], function(x) HD_func(consistent_maps$raster_data$original$count, x$count))
 
-sapply(UT_list[2:4], function(x) HD_func(UT_list[[1]], x))
-sapply(OV_list[2:4], function(x) HD_func(OV_list[[1]], x))
-sapply(NB_list[2:4], function(x) HD_func(NB_list[[1]], x))
 
 
 
 
 
 
-normal_maps$raster_data
 
 
 
